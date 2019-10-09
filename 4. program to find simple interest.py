795458671e3487b595f778168f870f2eab0f141c
Jhaggar <riteshjhaggar1988@gmail.com>
2019-10-09 16:09:47 +0530

    First commit

---
+++ 4. program to find simple interest.py
@@ -0,0 +1,11 @@
+# Python3 program to find simple interest
+
+P = 1 
+R = 1 
+T = 1 
+  
+# Calculates simple interest  
+SI = (P * R * T) / 100
+  
+# Print the resultant value of SI  
+print("simple interest is", SI) 
